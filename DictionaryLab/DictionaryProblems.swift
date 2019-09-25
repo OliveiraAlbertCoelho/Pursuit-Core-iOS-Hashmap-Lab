ca1020cf9f3663eb710a88f6b34fe1ba679d118c
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-09-25 18:11:09 -0400

    some more questions done

--- DictionaryLab/DictionaryProblems.swift
+++ DictionaryLab/DictionaryProblems.swift
@@ -25,13 +25,34 @@ func mostFrequentlyOccurringChar(in str: String) -> Character {
 // Find the first indicies whose values sum to a given number
 
 func pairSum(arr: [Int], target: Int) -> (Int, Int) {
+    
     return (0, 0)
+    
 }
 
 // Find the second most frequently occurring character in a given string
 
 func secondMostFrequentlyOccurringChar(in str: String) -> Character {
-    return "b"
+    let biggest = mostFrequentlyOccurringChar(in: str)
+    var myDict = [Character:Int]()
+    var counter = -1
+    var myKey:Character = "c"
+    for i in str{
+        if i != biggest{
+            if myDict.keys.contains(i){
+                myDict[i]! += 1
+            }else {
+                myDict[i] = 1
+            }
+        }
+    }
+    for (k,v) in myDict{
+        if counter < v{
+            counter = v
+            myKey = k
+        }
+    }
+    return myKey
 }
 
 // https://leetcode.com/problems/ransom-note/
@@ -52,14 +73,39 @@ func canConstruct(note: String, from originalLetters: String) -> Bool {
 // Return whether or not an input array contains duplicates
 
 func containsDuplicates(arr: [Int]) -> Bool {
+    var myDict = [Int:Int]()
+    for i in arr{
+        if myDict.keys.contains(i){
+            myDict[i]! += 1
+            return true
+        }else {
+            myDict[i] = 1
+        }
+    }
+    
     return false
 }
 
 // Find all values that appear exactly once in a given array of Strings
 
 func uniqueValues(in arr: [String]) -> [String] {
-    return []
+    var myDict = [String:Int]()
+    var myArray = [String]()
+    for i in arr{
+        if myDict.keys.contains(i){
+            myDict[i]! += 1
+        }else {
+            myDict[i] = 1
+        }
+    }
+    for (k,v) in myDict{
+        if v == 1{
+        myArray.append(k)
+      }
+    }
+    return myArray
 }
+
 
 // Sort a given array by how often each term appears
 
